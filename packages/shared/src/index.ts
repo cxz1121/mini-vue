8fbf7b6c620796ced6e0c1d3d7abbecad5de93bf
cxz1121 <1217044926@qq.com>
2022-08-12 11:33:08 +0800

    shared function

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,19 @@
 export function isObject(value) {
   return typeof value === 'object' && value !== null
 }
+
+export function isString(value) {
+  return typeof value === 'string'
+}
+
+export function isNumber(value) {
+  return typeof value === 'number'
+}
+
+export function isFunction(value) {
+  return typeof value === 'function'
+}
+
+export const isArray = Array.isArray
+
+export const assign = Object.assign
