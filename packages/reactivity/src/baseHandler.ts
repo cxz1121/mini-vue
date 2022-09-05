0f3808a38219f6a1b948fcb24d1b05bd56c12597
cxz1121 <1217044926@qq.com>
2022-09-05 16:40:10 +0800

    reconstruction reactivity

--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -1,23 +1,70 @@
+import { isObject } from '@vue/shared';
 import { track, trigger } from "./effect"
+import { reactive, readonly } from './reactive';
 
 export const enum ReactiveFlags {
-  isReactive = '__v_isReactive'
+  IS_REACTIVE = '__v_isReactive',
+  IS_READONLY = '__v_isReadonly'
+}
+
+const get = createGetter()
+const set = createSetter()
+const readonlyGet = createGetter(true)
+const readonlySet = createSetter(true)
+const shallowReadonlyGet = createGetter(true, true)
+const shallowReadonlySet = readonlySet
+
+function createGetter(isReadonly = false, shallow = false) {
+  return function get(target, key, receiver) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly
+    }
+    const res = Reflect.get(target, key, receiver)
+    if(shallow) {
+      return res
+    }
+    if (!isReadonly) {
+      track(target, key, 'get')
+    }
+    if(isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res)
+    }
+    return res
+  }
+}
+
+function createSetter(isReadonly = false, shallow = false) {
+  if(isReadonly) {
+    return function set(target, key, newValue, receiver) {
+      console.warn(`key: ${key} set fail, target is readonly`)
+      return true
+    }
+  } else {
+    return function set(target, key, newValue, receiver) {
+      const oldValue = target[key]
+      const res = Reflect.set(target, key, newValue, receiver)
+      if (oldValue !== newValue) {
+        //触发更新
+        trigger(target, key, oldValue, newValue, 'set')
+      }
+      return res
+    }
+  }
 }
 
 export const mutableHandler = {
-  get(target, key, receiver) {
-    if (key === ReactiveFlags.isReactive) {
-      return true
-    }
-    track(target, key, 'get')
-    return Reflect.get(target, key, receiver)
-  },
-  set(target, key, newValue, receiver) {
-    let oldValue = target[key]
-    let result = Reflect.set(target, key, newValue, receiver)
-    if (oldValue !== newValue) {
-      trigger(target, key, oldValue, newValue, 'set')
-    }
-    return result
-  }
+  get,
+  set
+}
+
+export const readonlyHandler = {
+  get: readonlyGet,
+  set: readonlySet
+}
+
+export const shallowReadonlyHandler = {
+  get: shallowReadonlyGet,
+  set: shallowReadonlySet
 }
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,26 +1,42 @@
 import { isObject } from "@vue/shared";
-import { mutableHandler, ReactiveFlags } from "./baseHandler";
+import { mutableHandler, ReactiveFlags, readonlyHandler, shallowReadonlyHandler } from "./baseHandler";
 
 const reactiveMap = new WeakMap()
+const readonlyMap = new WeakMap()
+const shallowReadonlyMap = new WeakMap()
 
 export function isReactive(value) {
-  return !!(value && value[ReactiveFlags.isReactive])
+  return !!(value && value[ReactiveFlags.IS_REACTIVE])
 }
 
-export function reactive(target) {
+export function isReadonly(value) {
+  return !!(value && value[ReactiveFlags.IS_READONLY])
+}
+
+export function isProxy(value) {
+  return isReactive(value) || isReadonly(value)
+}
+
+function createReactiveObject(target, baseHandler, proxyMap) {
   if (!isObject(target)) return
-
-  if (target[ReactiveFlags.isReactive]) {
-    return target
-  }
-
-  let exisitingProxy = reactiveMap.get(target)
+  if (isProxy(target)) return target
+  let exisitingProxy = proxyMap.get(target)
   if (exisitingProxy) {
     return exisitingProxy
   }
-  
-  const proxy = new Proxy(target, mutableHandler)
-
-  reactiveMap.set(target, proxy)
+  const proxy = new Proxy(target, baseHandler)
+  proxyMap.set(target, proxy)
   return proxy
+}
+
+export function reactive(target) {
+  return createReactiveObject(target, mutableHandler, reactiveMap)
+}
+
+export function readonly(target) {
+  return createReactiveObject(target, readonlyHandler, readonlyMap)
+}
+
+export function shallowReadonly(target) {
+  return createReactiveObject(target, shallowReadonlyHandler, shallowReadonlyMap)
 }
