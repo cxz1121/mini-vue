49eba441df10d1785a4bc95ea88f354c7a437f3e
cxz1121 <1217044926@qq.com>
2022-09-07 16:53:06 +0800

    patchElement patchProps

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1,5 +1,5 @@
 import { isString, ShapeFlags } from '@vue/shared';
-import { createVnode, Text } from './vnode';
+import { createVnode, isSameVnode, Text } from './vnode';
 export function createRenderer(renderOptions) {
     let {
         insert: hostInsert,
@@ -44,36 +44,76 @@ export function createRenderer(renderOptions) {
             }
         }
     }
+    const patchProps = (el, oldProps, newProps) => {
+        if(newProps) {
+            for (let key in newProps) {
+                hostPatchProp(el, key, oldProps[key], newProps[key])
+            }
+        }
+        for (let key in oldProps) {
+            if(!newProps[key]) {
+                hostPatchProp(el, key, oldProps[key], null)
+            }
+        }
+    }
+    const patchElement = (n1, n2, container) => {
+        let el = n2.el = n1.el
+        
+        let oldProps = n1.props || {}
+        let newProps = n2.props || {}
+
+        patchProps(el, oldProps, newProps)
+    }
+    const processElement = (n1, n2, container) => {
+        if(n1 === null) {
+            mountElement(n2, container)
+        } else {
+            //更新
+            patchElement(n1, n2, container)
+        }
+    }
     const processText = (n1, n2, container) => {
         if(n1 === null) {
             let el = n2.el = hostCreateText(n2.children)
             hostInsert(el, container)
         } else {
-
+            //更新
+            let el = n2.el = n1.el
+            if(n1.children !== n2.children) {
+                hostSetText(el, n2.children)
+            }
         }
     }
     const patch = (n1, n2, container) => {
         if(n1 === n2) return
-        const { type, shapeFlag } = n2
-        if(n1 == null) {
-            //初次渲染 mount
-            switch (type) {
-                case Text:
-                    processText(n1, n2, container)
-                    break;
-                default:
-                    if(shapeFlag & ShapeFlags.ELEMENT) {
-                        mountElement(n2, container)
-                    }
-                    break;
-            }
-        } else {
-            //更新
+
+        if(n1 && !isSameVnode(n1, n2)) {
+            unmount(n1)
+            n1 = null
         }
+
+        const { type, shapeFlag } = n2
+
+        switch (type) {
+            case Text:
+                processText(n1, n2, container)
+                break;
+            default:
+                if(shapeFlag & ShapeFlags.ELEMENT) {
+                    processElement(n1, n2, container)
+                }
+                break;
+        }
+    }
+    const unmount = (vnode) => {
+        hostRemove(vnode.el)
     }
     const render = (vnode, container) => {
         if(vnode == null) {
             //卸载操作
+            if(container._vnode) {
+                unmount(container._vnode)
+            }
         } else {
             //初始化 或者 更新
             patch(container._vnode || null, vnode, container)
--- packages/runtime-dom/src/modules/style.ts
+++ packages/runtime-dom/src/modules/style.ts
@@ -1,11 +1,21 @@
 export function patchStyle(el, preValue, newValue) {
-    for (let key in newValue) {
-        el.style[key] = newValue[key]
-    }
-    if(preValue) {
+    if(preValue && newValue) {
+        for (let key in newValue) {
+            el.style[key] = newValue[key]
+        }
         for(let key in preValue) {
-            if(newValue[key] == null) {
+            if(!newValue || newValue[key] == null) {
                 el.style[key] = null
+            }
+        }
+    } else {
+        if(preValue) {
+            for(let key in preValue) {
+                el.style[key] = null
+            }
+        } else if(newValue) {
+            for (let key in newValue) {
+                el.style[key] = newValue[key]
             }
         }
     }
